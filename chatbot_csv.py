b3efa7926aba037227f8e1533fb99080babc6ecd
Sarah Ouziel <perso.assistante@gmail.com>
2025-04-21 11:06:58 +0200

    data: mise à jour du fichier chatbot

--- chatbot_csv.py
+++ chatbot_csv.py
@@ -31,7 +31,7 @@ def enregistrer_visite():
         writer = csv.writer(f)
         writer.writerow([date_heure])
 
-        if os.path.exists(CSV_VISITE_FILE):
+    if os.path.exists(CSV_VISITE_FILE):
     with open(CSV_VISITE_FILE, "r") as f:
         contenu = f.read()
     st.code(contenu, language="csv")
