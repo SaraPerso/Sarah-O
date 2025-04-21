1f79b1c051cc29dc7ff2f45cc52affd7606b3297
Sarah Ouziel <perso.assistante@gmail.com>
2025-04-21 20:18:11 +0200

    data: mise à jour du fichier chatbot

--- chatbot_csv.py
+++ chatbot_csv.py
@@ -99,7 +99,7 @@ st.markdown("""
             font-size: 2.5em;
             font-weight: bold;
             text-align: center;
-            padding: 25px;
+            padding: 15px;
         }
     </style>
 """, unsafe_allow_html=True)
